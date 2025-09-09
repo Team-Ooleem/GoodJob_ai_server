ab9c8f3db82396085c13c0008bc120ebfcdffa04
omokgosu <anfakt0606@naver.com>
2025-09-10 06:19:41 +0900

    [ feat ] cors 설정 추가

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from app.routers.avatar import router as avatar_router
 origins = [
     "https://good-job.shop",
     "https://ai.good-job.shop", 
-    "https://appi.good-job.shop",
+    "https://api.good-job.shop",
     "https://localhost:3443",  # 개발 환경용
     "http://localhost:3000",   # 개발 환경용
     "http://localhost:8080",   # 개발 환경용
@@ -21,10 +21,11 @@ app.include_router(avatar_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,  # 허용할 origin
+    allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],  # 모든 메서드 허용 (GET, POST 등)
-    allow_headers=["*"],  # 모든 헤더 허용
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_headers=["*"],
+    expose_headers=["*"]
 )
 
 # app.mount("/static", StaticFiles(directory="static"), name="static")
